25fc26cd324322b73f39dc379532177722b5c9bd
Christian Fries <email@christian-fries.de>
2018-07-18 10:23:07 +0300

    Added package description.

---
+++ src/main/java/net/finmath/initialmargin/isdasimm/aggregationscheme/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Aggregation of portfolio initial margin.
+ *
+ * @author Peter-Kohl-Landgraf
+ * @author Mario Viehmann
+ * @author Christian Fries
+ */
+package net.finmath.initialmargin.isdasimm.aggregationscheme;
---
+++ src/main/java/net/finmath/initialmargin/isdasimm/changedfinmath/products/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Classes adding some additional functionality to finmath-lib
+ *
+ * @author Mario Viehmann
+ * @author Christian Fries
+ */
+package net.finmath.initialmargin.isdasimm.changedfinmath.products;
