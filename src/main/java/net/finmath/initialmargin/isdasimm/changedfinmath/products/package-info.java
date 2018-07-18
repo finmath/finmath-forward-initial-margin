ca0eeb4338a8a188a5263041074d6b4475f46c5d
Christian Fries <email@christian-fries.de>
2018-07-18 22:57:13 +0300

    Clean up.

--- src/main/java/net/finmath/initialmargin/isdasimm/changedfinmath/products/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * Classes adding some additional functionality to finmath-lib
- *
- * @author Mario Viehmann
- * @author Christian Fries
- */
-package net.finmath.initialmargin.isdasimm.changedfinmath.products;
